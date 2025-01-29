b5c543d40dd2a71f69a25a9194a5083cbcc50e41
ZaydM18 <zayd.m18@gmail.com>
2025-01-30 01:23:06 +0800

    Level-2

--- src/main/java/Monty.java
+++ src/main/java/Monty.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 
 public class Monty {
-    private static void longDash() {
+    private static void printDash() {
         System.out.println("____________________________________________________________");
     }
     public static void main(String[] args) {
-        longDash();
-        System.out.println(" Hello! I'm Monty");
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
+
+        printDash();
+        System.out.println(" Hello! I'm Monty, your personal organisational assistant! If you need a list, I'll be the first to enlist!");
         System.out.println(" What can I do for you?");
-        longDash();
+        printDash();
 
         Scanner sc = new Scanner(System.in);
 
@@ -16,15 +19,35 @@ public class Monty {
             String userInput = sc.nextLine().trim();
 
             if (userInput.equalsIgnoreCase("bye")) {
-                longDash();
+                printDash();
                 System.out.println(" Bye. Hope to see you again soon!");
-                longDash();
+                printDash();
                 break;
             }
 
-            longDash();
-            System.out.println(" " + userInput);
-            longDash();
+            else if (userInput.equalsIgnoreCase("list")) {
+                printDash();
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                printDash();
+            }
+
+            else {
+                Task newTask = new Task(userInput);
+                tasks[taskCount] = newTask;
+                taskCount++;
+
+                printDash();
+                System.out.println(" added: " + newTask.getDescription());
+
+                if (userInput.toLowerCase().startsWith("read")) {
+                    System.out.println(" (Oh good, I thought nobody liked to read anymore!)");
+                }
+
+                printDash();
+            }
+
         }
 
     }
