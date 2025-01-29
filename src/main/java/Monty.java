eb792701a9d80eeabeaec052e3521b6e8ee32aec
ZaydM18 <zayd.m18@gmail.com>
2025-01-30 00:58:33 +0800

    Level-1

--- src/main/java/Monty.java
+++ src/main/java/Monty.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+
 public class Monty {
-    public static void main(String[] args) {
+    private static void longDash() {
         System.out.println("____________________________________________________________");
+    }
+    public static void main(String[] args) {
+        longDash();
         System.out.println(" Hello! I'm Monty");
         System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
+        longDash();
 
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            String userInput = sc.nextLine().trim();
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                longDash();
+                System.out.println(" Bye. Hope to see you again soon!");
+                longDash();
+                break;
+            }
+
+            longDash();
+            System.out.println(" " + userInput);
+            longDash();
+        }
+
     }
 }
