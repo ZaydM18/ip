606daa2cd77833b09aa9d3f88f33bcdfa56b12a1
ZaydM18 <zayd.m18@gmail.com>
2025-02-21 10:32:43 +0800

    Refactor code to better reflect Java Coding Standards
    To better reflect the course's coding standards, I had to make changes
    to my IDE's settings, especially to handle switch statements and method
    braces.
    
    Line breaks were more complicated to handle and required case-by-case
    oversight. The coding standard will need to be maintained for future
    commits.

--- src/test/java/monty/MontyTest.java
+++ src/test/java/monty/MontyTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class MontyTest {
--- src/test/java/monty/storage/StorageTest.java
+++ src/test/java/monty/storage/StorageTest.java
@@ -4,7 +4,9 @@ import monty.exception.MontyException;
 import monty.task.Task;
 import monty.task.ToDo;
 import org.junit.jupiter.api.Test;
+
 import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StorageTest {
